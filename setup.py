b2cb2f92d18fdda3035ba4b163066f2c80e1e866
soasme <soasme@gmail.com>
2016-06-16 13:38:29 +0800

    Bumpversion 0.1.3 -> 0.1.4.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from os.path import join, dirname
 
 setup_args = {
     'name': 'scrapyd_kit',
-    'version': '0.1.3',
+    'version': '0.1.4',
     'url': 'https://github.com/soasme/scrapyd_kit',
     'description': 'A kit for extending Scrapyd',
     'long_description': open('README.rst').read(),
